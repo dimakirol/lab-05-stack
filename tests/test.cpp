d1a56732b4838579ce1bb43b3a8fd96fb1fffc2f
olkravi <olkravi1580@gmail.com>
2020-02-16 14:00:24 +0300

    16.02.2020 14:00

--- tests/test.cpp
+++ tests/test.cpp
@@ -23,27 +23,27 @@ TEST(Example, EmptyTest)
   EXPECT_EQ(test.pop(), 2);
 }
 
-// TEST(stack_t, Text)
-// {
-//   stack_st<int> test;
-//   test.push_emplace(1);
-//   test.push_emplace(3);
-//   test.push(2);
-//   test.push(3);
-//   test.push(4);
-//   test.push(12);
+TEST(stack_t, Text)
+{
+  stack_st<int> test;
+  test.push_emplace(1);
+  test.push_emplace(3);
+  test.push(2);
+  test.push(3);
+  test.push(4);
+  test.push(12);
 
 //   EXPECT_EQ(std::is_move_assignable<stack_st<int>>::value, true);
 //   EXPECT_EQ(std::is_move_constructible<stack_st<int>>::value, true);
 
-//   EXPECT_EQ(test.head(), 12);
-//   EXPECT_EQ(test.pop(), 12);
-//   EXPECT_EQ(test.pop(), 4);
-//   EXPECT_EQ(test.pop(), 3);
-//   EXPECT_EQ(test.pop(), 2);
-//   EXPECT_EQ(test.pop(), 3);
-//   EXPECT_EQ(test.pop(), 1);
-// }
+  EXPECT_EQ(test.head(), 12);
+  EXPECT_EQ(test.pop(), 12);
+  EXPECT_EQ(test.pop(), 4);
+  EXPECT_EQ(test.pop(), 3);
+  EXPECT_EQ(test.pop(), 2);
+  EXPECT_EQ(test.pop(), 3);
+  EXPECT_EQ(test.pop(), 1);
+}
 
 int main(int argc, char **argv)
 {

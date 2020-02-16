e3f73e1f54beb58fb482541633fab4d43d55b806
olkravi <olkravi1580@gmail.com>
2020-02-16 11:54:20 +0300

    16.02.2020 11:54

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,52 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "header.hpp"
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(stack, Text)
+{
+    stack<int> test;
+    int a = 2, b = 3, c = 4;
+
+    test.push(a);
+    test.push(b);
+    test.push(c);
+    test.push(12);
+
+    EXPECT_EQ(std::is_move_assignable<stack<int>>::value, true);
+    EXPECT_EQ(std::is_move_constructible<stack<int>>::value, true);
+
+    EXPECT_EQ(test.head(), 12);
+    EXPECT_EQ(test.pop(), 12);
+    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 2);
+}
+
+TEST(stack_t, Text)
+{
+    stack_st<int> test;
+    test.push_emplace(1);
+    test.push_emplace(3);
+    test.push(2);
+    test.push(3);
+    test.push(4);
+    test.push(12);
+
+    EXPECT_EQ(std::is_move_assignable<stack_st<int>>::value, true);
+    EXPECT_EQ(std::is_move_constructible<stack_st<int>>::value, true);
+
+    EXPECT_EQ(test.head(), 12);
+    EXPECT_EQ(test.pop(), 12);
+    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 2);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 1);
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
